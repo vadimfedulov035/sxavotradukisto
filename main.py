44c8636934ce5f8d177c05665a38a3f2f8b3f630
Vadim <vadimfedulov035@gmail.com>
2023-10-23 15:51:42 +0000

    v1.4

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ import telebot
 with open("token.cfg", "r", encoding="utf-8") as f:
     token = f.read().rstrip()
 
+
 with open("bazvortaro.json", "r", encoding="utf-8") as f:
     sxava_bazvortaro = json.load(f)
-    latina_bazvortaro = {v: s for k, v in sxava_bazvortaro.items()}
+    latina_bazvortaro = {v: k for k, v in sxava_bazvortaro.items()}
+    sxava_bazvortaro_granda = {f"·{k}": v.upper() for k, v in sxava_bazvortaro.items()}
+    latina_bazvortaro_granda = {k.upper(): f"·{v}" for k, v in latina_bazvortaro.items()}
 
 
 with open("reformvortaro.json", "r", encoding="utf-8") as f:
     sxava_reformvortaro = json.load(f)
-    latina_reformvortaro = {v: s for k, v in sxava_reformvortaro.items()}
+    latina_reformvortaro = {v: k for k, v in sxava_reformvortaro.items()}
+    sxava_reformvortaro_granda = {f"·{k}": v.capitalize() for k, v in sxava_reformvortaro.items()}
+    latina_reformvortaro_granda = {k.capitalize(): f"·{v}" for k, v in latina_reformvortaro.items()}
 
 
 bot = telebot.TeleBot(token)
@@ -25,29 +30,32 @@ bot = telebot.TeleBot(token)
 def divenu_lingvon(teksto):
     mia_kalkulilo = Counter(teksto)
     sxavofto, latinofto = 0, 0
-    for k, v in sxava_sercxotablo.items():
-        sxavofteco += mia_kalkulilo[k]
-        latinofteco += mia_kalkulilo[v]
-    diveno = "sxava" if sxavofteco >= latinofteco else "latina"
+    for k, v in sxava_bazvortaro.items():
+        sxavofto += mia_kalkulilo[k]
+        latinofto += mia_kalkulilo[v]
+    diveno = "sxava" if sxavofto >= latinofto else "latina"
     return diveno
 
 
 def traduki(teksto, vortaro):
     for k, v in vortaro.items():
-        teksto = re.sub(f"·{k}", v.upper(), teksto)
         teksto = re.sub(k, v, teksto)
     return teksto
 
 
 def latinigi(teksto):
-    teksto = traduki(sxava_reformvortaro)
-    teksto = traduki(sxava_bazvortaro)
+    teksto = traduki(teksto, sxava_reformvortaro_granda)
+    teksto = traduki(teksto, sxava_bazvortaro_granda)
+    teksto = traduki(teksto, sxava_reformvortaro)
+    teksto = traduki(teksto, sxava_bazvortaro)
     return teksto
 
 
 def sxavigi(teksto):
-    teksto = traduki(latina_reformvortaro)
-    teksto = traduki(latina_bazvortaro)
+    teksto = traduki(teksto, latina_reformvortaro_granda)
+    teksto = traduki(teksto, latina_bazvortaro_granda)
+    teksto = traduki(teksto, latina_reformvortaro)
+    teksto = traduki(teksto, latina_bazvortaro)
     return teksto
 
 
